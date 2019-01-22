b261c5cae88ce874b142f73f94a1507cbc0c499c
Ambrose Hundal <amshunda@ucsc.edu>
2019-01-22 15:41:53 -0800

    testing constructors java

---
+++ apint.java
@@ -0,0 +1,49 @@
+//import any?
+
+
+public class apint{
+
+    public int ap_array[];
+
+    //default constructor
+    public apint(){
+        this.ap_array = null;
+        System.out.println("default constructor, apint created");
+    }
+
+    //constructor using strings
+    public apint(String s){
+
+    }
+
+    //constructor for conversion of ints
+    public apint(int convert){
+
+    }
+
+    //constructor for conversion of reals that truncates the fractional part
+
+
+
+    //method to print
+
+
+    //methods for addition, subtraction, multiplication and division
+
+
+    //main
+    public static void main(String args[]){
+
+        //testing for default constructor
+        apint a = new apint();
+
+        apint b = new apint();
+
+        //testing for constructor using strings
+        
+
+       
+
+    }
+    
+}
